ed37a52fd2b538970436e0e890b6eff7f0d65419
Sakshi <sakshi.jain7597@gmail.com>
2022-10-18 00:02:35 +0530

    Updated learn more page content and added a back button

--- Web/clients/amb-driver/src/components/LearnMore.js
+++ Web/clients/amb-driver/src/components/LearnMore.js
@@ -1,15 +1,12 @@
+import { Link } from "react-router-dom";
+
 const LearnMore = () => {
   return (
     <div>
-      This web app assists hospitals by bridging the communication gap between
-      Ambulance driver and hospital management especially during emergency
-      cases. This web app successfully tracks and alerts hospital management by
-      which delay in treatment of patient can be reduced. This could potentially
-      save lives. This webapp notifies hospital when an ambulance is within 3km
-      range of a hospital. This will be very useful at times of emergency cases
-      which assists in timely treatement of patients. This works by tracking
-      ambulance location(coordinates) in real-time. The location stats will also
-      be updated in realtime on hospital's display board.
-    </div>
+      Signing up drivers can only be done by Administrators. To add a user, please contact the administrator of this project at <a href="https://yaswanth820.github.io/my-portfolio-site/">Link</a>.
+    <Link to="/signup"><button>Back</button></Link>
+	</div>
   );
 };
+
+export default LearnMore;
